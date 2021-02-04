eb132a96a9fd89ea7219720c3d5e9a58cc1742c6
Sam Baynham <sam.baynham@gmail.com>
2021-02-04 10:00:13 +0000

    Working mixes

--- src/Console/Commands/CreateDomainEntityCommand.php
+++ src/Console/Commands/CreateDomainEntityCommand.php
@@ -2,16 +2,17 @@
 
 namespace Fifthgate\Objectivity\Repositories\Console\Commands;
 
-use Illuminate\Console\Command;
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Str;
 
-class CreateDomainEntityCommand extends Command
+class CreateDomainEntityCommand extends GeneratorCommand
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'make:objectivity-domain-entity';
+    protected $signature = 'make:objectivity-domain-entity {entityName} {--withMigration}';
 
     /**
      * The console command description.
@@ -37,6 +38,39 @@ class CreateDomainEntityCommand extends Command
      */
     public function handle()
     {
+        $entityName = $this->argument('entityName');
+        $withMigration = $this->option('withMigration');
+
+        if ($withMigration) {
+            $this->createMigration();
+        }
+        $this->createEntity($entityName);
         return 0;
+    }
+
+    protected function createMigration() {
+        $table = Str::snake(Str::pluralStudly(class_basename($this->argument('entityName'))));
+        $this->call('make:migration', [
+            'name' => "create_{$table}_table",
+            '--create' => $table,
+        ]);
+    }
+
+    protected function createEntity(string $entityName) {
+        var_dump($this->laravel->basePath($entityName));
+        die("Hello World");
+    }
+
+    /**
+     * Resolve the fully-qualified path to the stub.
+     *
+     * @param  string  $stub
+     * @return string
+     */
+    protected function resolveStubPath($stub)
+    {
+        return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
+                        ? $customPath
+                        : __DIR__.$stub;
     }
 }
