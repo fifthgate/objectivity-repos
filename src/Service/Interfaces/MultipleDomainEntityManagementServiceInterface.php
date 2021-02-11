3f3a739e2dcf680942a8ddfa685933fa526fd7ab
Sam Baynham <sam@fifthgate.net>
2021-02-11 16:01:59 +0000

    Modifying interface to allow for non-numeric ids

--- src/Service/Interfaces/MultipleDomainEntityManagementServiceInterface.php
+++ src/Service/Interfaces/MultipleDomainEntityManagementServiceInterface.php
@@ -7,11 +7,11 @@ use Fifthgate\Objectivity\Core\Domain\Collection\Interfaces\DomainEntityCollecti
 
 interface MultipleDomainEntityManagementServiceInterface {
 
-	public function find(string $type, int $id) : ? DomainEntityInterface;
+	public function find(string $type, $id) : ? DomainEntityInterface;
 
-    public function findDeleted(string $type, int $id) : ? DomainEntityInterface;
+    public function findDeleted(string $type, $id) : ? DomainEntityInterface;
 
-    public function findMany(string $type, array $fids) : ? DomainEntityCollectionInterface;
+    public function findMany(string $type, $ids) : ? DomainEntityCollectionInterface;
 
     public function findAllOfType(string $type) : ? DomainEntityCollectionInterface;
 }
